5291df1553522f930e321dbd69300a62c986cad4
kamran-shaukat <xs2kami@gmail.com>
2024-10-29 22:01:10 +0500

    added one more test for demo example and change status code on api test

---
+++ tests/kamran.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Test Case 1', async ({ page }) => {
+
+
+  // Expect 
+expect(10).toBe(10)
+});
+
+test('Test Case 2', async ({ page }) => {
+
+
+  // Expect 
+expect(11).toBe(10)
+});
--- tests/recorded.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/search?q=playwright&sca_esv=e8313a84c6056b2f&source=hp&ei=RScMZ8jrIN-H4-EP95e0yA4&iflsig=AL9hbdgAAAAAZww1VSbkmOxZHg-XEcB1Rm5xzwp0s7O5&ved=0ahUKEwiIovORk4yJAxXfwzgGHfcLDekQ4dUDCA0&uact=5&oq=&gs_lp=Egdnd3Mtd2l6IgBIAFAAWABwAHgAkAEAmAEAoAEAqgEAuAEDyAEAmAIAoAIAmAMAkgcAoAcA&sclient=gws-wiz');
-  await page.getByRole('link', { name: 'Playwright: Fast and reliable' }).click();
-  await page.getByRole('link', { name: 'Get started' }).click();
-});
